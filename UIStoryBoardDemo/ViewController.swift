b74e551beced49e0a6c6aefc24dc09d64e3dda63
swordjoy <swordjooy@gmail.com>
2018-01-26 19:56:26 +0800

    Initial Commit

---
+++ UIStoryBoardDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIStoryBoardDemo
+//
+//  Created by Liu Yang on 2018/1/26.
+//  Copyright © 2018年 Liu Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
