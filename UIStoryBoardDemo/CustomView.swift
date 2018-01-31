09c3978f4ba89a713174ccdf6caf852327d2ee0c
swordjoy <swordjooy@gmail.com>
2018-01-31 23:01:45 +0800

    first commit

---
+++ UIStoryBoardDemo/CustomView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  UIStoryBoardDemo
+//
+//  Created by Liu Yang on 2018/1/31.
+//  Copyright © 2018年 Liu Yang. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ UIStoryBoardDemo/Tool/Extension.swift
@@ -0,0 +1,9 @@
+//
+//  Extension.swift
+//  UIStoryBoardDemo
+//
+//  Created by Liu Yang on 2018/1/31.
+//  Copyright © 2018年 Liu Yang. All rights reserved.
+//
+
+import Foundation
